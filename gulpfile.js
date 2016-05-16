1752debf560949d5d01097efa29816f512c8c41b
Jon <jonnmb@Gmail.com>
2016-05-16 14:18:09 -0600

    more setup

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+// We will want to look into using gulp
+// Checkout Chases gulpfile.js and this website brandonclapp.com/what-is-gulp-js-and-why-use-it/
+
+var gulp = require('gulp');
+
+
+gulp.task('hello-world', function(){
+	console.log('HELLO WORLD');
+});
+
+
