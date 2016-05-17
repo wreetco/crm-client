644ac9327672117aead70ef0b664b56a8a597762
Jon <jonnmb@Gmail.com>
2016-05-17 15:35:52 -0600

    in theory gulp should compile sass changes while serving now

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,19 @@
 // Checkout Chases gulpfile.js and this website brandonclapp.com/what-is-gulp-js-and-why-use-it/
 
 var gulp = require('gulp');
-
-
+var sass = require('gulp-sass');
 var start = require('gulp-connect');
 
+gulp.task('styles', function() {
+	gulp.src('./client/assets/materialize-src/sass/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./client/assets/materialize-src/css/'));
+});
+
 gulp.task('start', function () {
+	gulp.watch('sass/**/*.scss',['styles']);
 	start.server({
-		root: 'client/',
+		root: './client/',
 		port: 1337,
 		hostname: '0.0.0.0'
 	});
