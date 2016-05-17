7d5e01154774a6e3a5ba806cfec78226e4f20183
Jon <jonnmb@Gmail.com>
2016-05-17 17:20:27 -0600

    some css is happening

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,31 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var start = require('gulp-connect');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var striplog = require('gulp-strip-debug');
+var gutil = require('gulp-util');
+
 
 gulp.task('styles', function() {
-	gulp.src('./client/assets/materialize-src/sass/*.scss')
+	var css_src = './client/assets/materialize-src/sass/*.scss';
+	var css_dest = './client/assets/css';
+
+	gulp.src(css_src)
 		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('./client/assets/css/'));
+		.pipe(gulp.dest(css_dest));
+});
+
+gulp.task('scripts', function() {
+	var js_src = './client/assets/materialize-src/js/*.js';
+	var js_dest = './client/assets/js';
+
+	gulp.src(js_src)
+		.pipe(concat('materialize.min.js'))
+		.pipe(striplog())
+		.pipe(uglify())
+		.pipe(gulp.dest(js_dest))
+		.on('error', gutil.log);
 });
 
 gulp.task('start', function () {
