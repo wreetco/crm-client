fbfe0bb5a1e23f9788acd4c5a44c0e91cca8b1a5
Jon <jonnmb@Gmail.com>
2016-05-17 12:58:49 -0600

    Things are coming along

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -1,1 +1,55 @@
-// app.js
+(function() {
+	'use strict';
+
+	angular.module('application', ['ui.materialize', 'ngRoute', 'ngAnimate'])
+
+	.config([
+		'$locationProvider',
+		'$routeProvider',
+		function($locationProvider, $routeProvider) {
+		$locationProvider.hashPrefix('!');
+		//routes
+		$routeProvider
+			.when("/", {
+				templateUrl: "./views/contacts.html",
+				controller: "ContactController"
+			})
+			.when("/settings", {
+				templateUrl: "./views/settings.html",
+				controller: "SettingsController"
+			})
+			.when("/statistics", {
+				templateUrl: "./views/statistics.html",
+				controller: "StatisticsController"
+			})
+			.otherwise({
+				redirectTo: '/'
+			});
+		}
+	])
+
+	//controllers
+
+	.controller('ContactController', [
+		'$scope',
+		function($scope) {
+			$scope.test = "Test Output Contact page";
+		}
+	])
+
+	.controller('SettingsController', [
+		'$scope',
+		function($scope) {
+			$scope.test = "Test Output Settings Page";
+		}
+	])
+        .controller('StatisticsController', [
+                '$scope',
+                function($scope) {
+                        $scope.test = "Test Output Statistics Page";
+                }
+        ]);
+}());
+
+
+
