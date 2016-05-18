df2c3b8f03abac2af4745456b2c9d790bd19eb68
Jon <jonnmb@Gmail.com>
2016-05-18 02:04:13 -0600

    Gulp functionality

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,8 @@ var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var striplog = require('gulp-strip-debug');
 var gutil = require('gulp-util');
+var browserSync = require('browser-sync').create();
+var runSequence = require('run-sequence');
 
 
 //This compiles the sass files in the materialize folder
@@ -18,7 +20,10 @@ gulp.task('styles', function() {
 
 	gulp.src(css_src)
 		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest(css_dest));
+		.pipe(gulp.dest(css_dest))
+		.pipe(browserSync.reload({
+      		stream: true
+    	}));
 });
 
 //This grabs all the javascript files from materialize
@@ -32,9 +37,36 @@ gulp.task('scripts', function() {
 		.pipe(striplog())
 		.pipe(uglify())
 		.pipe(gulp.dest(js_dest))
-		.on('error', gutil.log);
+		.on('error', gutil.log)
+		.pipe(browserSync.reload({
+      		stream: true
+    	}));
 });
 
+//This should catch *most* project changes and automagically reload your browser, neato
+gulp.task('watch', ['browserSync', 'styles', 'scripts'], function(){
+	var js_src = './client/assets/materialize-src/js/*.js';
+	var css_src = './client/assets/materialize-src/sass/**/*.scss';
+	var index_src ="./client/index.html";
+	var views_src ="./client/views/*.html";
+
+  	gulp.watch(js_src, ['scripts']); 
+  	gulp.watch(css_src, ['styles']);
+  	gulp.watch(index_src, browserSync.reload);
+  	gulp.watch(views_src, browserSync.reload);
+  
+})
+
+//Reload browser when you make a change to sass or js
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: './client'
+    },
+  })
+})
+
+//Starts the server on the localhost:1337
 gulp.task('start', function () {
 	start.server({
 		root: './client/',
@@ -42,3 +74,10 @@ gulp.task('start', function () {
 		hostname: '0.0.0.0'
 	});
 });
+
+//Start the server and watch for changes to do live reload
+gulp.task('run', function (callback) {
+  runSequence(['start', 'watch'], 
+    callback
+  )
+})
