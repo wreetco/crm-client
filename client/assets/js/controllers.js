57ec9b0ba582e1e8b8bb1eec27212aa605d83d1e
Jon <jonnmb@Gmail.com>
2016-05-21 19:24:01 -0600

    versioning

--- client/assets/js/controllers.js
+++ client/assets/js/controllers.js
@@ -12,7 +12,7 @@ angular.module('application.controllers', ['nvd3'])
     //This test variable is output on the page with interpolateion {{}}
     $scope.test = "Test Output Statistics Page";
 
-    //this defines the graph on the stats page
+    //This defines the graph on the stats page
     $scope.options = {
         chart: {
             type: 'lineChart',
@@ -102,5 +102,60 @@ angular.module('application.controllers', ['nvd3'])
             }
         ];
     };
+
+
+
+
+    $scope.options2 = {
+        chart: {
+            type: 'pieChart',
+            height: 500,
+            x: function(d){return d.key;},
+            y: function(d){return d.y;},
+            showLabels: true,
+            duration: 500,
+            labelThreshold: 0.01,
+            labelSunbeamLayout: true,
+            legend: {
+                margin: {
+                    top: 5,
+                    right: 35,
+                    bottom: 5,
+                    left: 0
+                }
+            }
+        }
+    };
+
+    $scope.data2 = [
+        {
+            key: "One",
+            y: 5
+        },
+        {
+            key: "Two",
+            y: 2
+        },
+        {
+            key: "Three",
+            y: 9
+        },
+        {
+            key: "Four",
+            y: 7
+        },
+        {
+            key: "Five",
+            y: 4
+        },
+        {
+            key: "Six",
+            y: 3
+        },
+        {
+            key: "Seven",
+            y: .5
+        }
+    ];
   }])
 ;
