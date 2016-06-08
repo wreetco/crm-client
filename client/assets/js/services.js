66b5b477f8441183c276a84f7f2140798cd4a2a8
Chase Higgins <chasehx@gmail.com>
2016-06-07 20:11:15 -0400

    let lighttable take over indent scheme

--- client/assets/js/services.js
+++ client/assets/js/services.js
@@ -1,53 +1,50 @@
 angular.module('application.services', [])
-
-  .factory('httpRequestInterceptor', function($window) {
-    return {
-      request: function($config) {
-        if(JSON.parse(window.sessionStorage.session).token) {
-          $config.headers.Authorization = JSON.parse(window.sessionStorage.session).token;
-        }
-        return $config;
+.factory('httpRequestInterceptor', function($window) {
+  return {
+    request: function($config) {
+      if(JSON.parse(window.sessionStorage.session).token) {
+        $config.headers.Authorization = JSON.parse(window.sessionStorage.session).token;
       }
-    };
-  })
+      return $config;
+    }
+  };
+})
 
+.factory('Accounts', function($http) {
+  return {
+    login: function(email, passwd) {
+      return new Promise(function(resolve, reject) {
+        var req = JSON.stringify( { email, passwd });
+        $http.post("http://burnsy.wreet.xyz/auth", req)
+        .success(function(data, status) {
+          // future reqs will want the token
+          resolve(data);
+        })
+        .error(function(data, status) {
+          reject({data,status});
+        });
+      }); // end promise
 
-	.factory('Accounts', function($http) {
-		return {
-			login: function(email, passwd) {
-				return new Promise(function(resolve, reject) {
-					var req = JSON.stringify( { email, passwd });
+    }, // end login method
+  };
+}) // end accounts service
 
-					$http.post("http://burnsy.wreet.xyz/auth", req)
-						.success(function(data, status) {
-              // future reqs will want the token
-							resolve(data);
-					})
-						.error(function(data, status) {
-							reject({data,status});
-				   	});
-				}); // end promise
-
-			}, // end login method
-		};
-	}) // end accounts service
-
-  .factory('Manager', function($http) {
-    return {
-      getInterface: function(manager_id) {
-        return new Promise(function(resolve, reject) {
-          $http.get("http://burnsy.wreet.xyz/manager/"+manager_id+"/interface").success(function(data) {
-            resolve(data);
-          }).error(function(data, status) {
-            reject({
-              data: data,
-              status: status
-            });
+.factory('Manager', function($http) {
+  return {
+    getInterface: function(manager_id) {
+      return new Promise(function(resolve, reject) {
+        $http.get("http://burnsy.wreet.xyz/manager/"+manager_id+"/interface").success(function(data) {
+          resolve(data);
+        }).error(function(data, status) {
+          reject({
+            data: data,
+            status: status
           });
         });
-      } // end getInterface method
-    }; // end ret
-  }) // end Manager service
+      }); // end promise
+    } // end getInterface method
+  }; // end ret
+}) // end Manager service
 
 
 ;
