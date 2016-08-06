304c1c393e73cdae0f0d212668c90a175cfa37ec
Chase Higgins <chasehx@gmail.com>
2016-08-06 23:08:34 +0000

    stuff

--- client/assets/js/controllers.js
+++ client/assets/js/controllers.js
@@ -613,6 +613,21 @@ angular.module('application.controllers', ['nvd3'])
 
   };
 
+  $scope.filterContactsByTag = function(tag) {
+    if (!$scope.contacts) return -1;
+    var contacts = [];
+    for (var i = 0; i < $scope.contacts.length; i++) {
+      var c = $scope.contacts[i];
+      for (var j = 0; j < c.tags.length; j++) {
+        if (c.tags[j].name == tag) {
+          contacts.push(c);
+          break;
+        }
+      }
+    }
+    $scope.contacts = contacts;
+  }; // end filterBytag whatev methi
+
   ///////////////////////////////////////////////////////////////
   (function() {
     if (!$scope.contacts) {
@@ -632,10 +647,12 @@ angular.module('application.controllers', ['nvd3'])
           console.log(err);
         });
       }
+      $scope.tags = Interface.getTags($scope.contacts);
     } // end contact check
-    if ($routeParams.tag) {
-
-    }
+    $scope.$on('$routeChangeSuccess', function(next, current) { 
+      if ($routeParams.tag)
+        $scope.filterContactsByTag($routeParams.tag);
+    });
   })();
 
 }]) // end ContactController
@@ -760,150 +777,148 @@ angular.module('application.controllers', ['nvd3'])
   };
 }])
 
-.controller('StatisticsController',
-            ['$scope',
-             function($scope) {
-               //This defines the graph on the stats page
-               $scope.options = {
-                 chart: {
-                   type: 'lineChart',
-                   height: 450,
-                   margin : {
-                     top: 20,
-                     right: 20,
-                     bottom: 40,
-                     left: 55
-                   },
-                   x: function(d){ return d.x; },
-                   y: function(d){ return d.y; },
-                   useInteractiveGuideline: true,
-                   dispatch: {
-                     stateChange: function(e){ console.log("stateChange"); },
-                     changeState: function(e){ console.log("changeState"); },
-                     tooltipShow: function(e){ console.log("tooltipShow"); },
-                     tooltipHide: function(e){ console.log("tooltipHide"); }
-                   },
-                   xAxis: {
-                     axisLabel: 'Time (ms)'
-                   },
-                   yAxis: {
-                     axisLabel: 'Voltage (v)',
-                     tickFormat: function(d){
-                       return d3.format('.02f')(d);
-                     },
-                     axisLabelDistance: -10
-                   },
-                   callback: function(chart){
-                     console.log("!!! lineChart callback !!!");
-                   }
-                 },
-                 title: {
-                   enable: true,
-                   text: 'Title for Line Chart'
-                 },
-                 subtitle: {
-                   enable: true,
-                   text: 'Subtitle for simple line chart. Lorem ipsum dolor sit amet, at eam blandit sadipscing, vim adhuc sanctus disputando ex, cu usu affert alienum urbanitas.',
-                   css: {
-                     'text-align': 'center',
-                     'margin': '10px 13px 0px 7px'
-                   }
-                 },
-                 caption: {
-                   enable: true,
-                   html: '<b>Figure 1.</b> Lorem ipsum dolor sit amet, at eam blandit sadipscing, <span style="text-decoration: underline;">vim adhuc sanctus disputando ex</span>, cu usu affert alienum urbanitas. <i>Cum in purto erat, mea ne nominavi persecuti reformidans.</i> Docendi blandit abhorreant ea has, minim tantas alterum pro eu. <span style="color: darkred;">Exerci graeci ad vix, elit tacimates ea duo</span>. Id mel eruditi fuisset. Stet vidit patrioque in pro, eum ex veri verterem abhorreant, id unum oportere intellegam nec.',
-                   css: {
-                     'text-align': 'justify',
-                     'margin': '10px 13px 0px 7px'
-                   }
-                 }
-               };
+.controller('StatisticsController', ['$scope', function($scope) {
+   //This defines the graph on the stats page
+   $scope.options = {
+     chart: {
+       type: 'lineChart',
+       height: 450,
+       margin : {
+         top: 20,
+         right: 20,
+         bottom: 40,
+         left: 55
+       },
+       x: function(d){ return d.x; },
+       y: function(d){ return d.y; },
+       useInteractiveGuideline: true,
+       dispatch: {
+         stateChange: function(e){ console.log("stateChange"); },
+         changeState: function(e){ console.log("changeState"); },
+         tooltipShow: function(e){ console.log("tooltipShow"); },
+         tooltipHide: function(e){ console.log("tooltipHide"); }
+       },
+       xAxis: {
+         axisLabel: 'Time (ms)'
+       },
+       yAxis: {
+         axisLabel: 'Voltage (v)',
+         tickFormat: function(d){
+           return d3.format('.02f')(d);
+         },
+         axisLabelDistance: -10
+       },
+       callback: function(chart){
+         console.log("!!! lineChart callback !!!");
+       }
+     },
+     title: {
+       enable: true,
+       text: 'Title for Line Chart'
+     },
+     subtitle: {
+       enable: true,
+       text: 'Subtitle for simple line chart. Lorem ipsum dolor sit amet, at eam blandit sadipscing, vim adhuc sanctus disputando ex, cu usu affert alienum urbanitas.',
+       css: {
+         'text-align': 'center',
+         'margin': '10px 13px 0px 7px'
+       }
+     },
+     caption: {
+       enable: true,
+       html: '<b>Figure 1.</b> Lorem ipsum dolor sit amet, at eam blandit sadipscing, <span style="text-decoration: underline;">vim adhuc sanctus disputando ex</span>, cu usu affert alienum urbanitas. <i>Cum in purto erat, mea ne nominavi persecuti reformidans.</i> Docendi blandit abhorreant ea has, minim tantas alterum pro eu. <span style="color: darkred;">Exerci graeci ad vix, elit tacimates ea duo</span>. Id mel eruditi fuisset. Stet vidit patrioque in pro, eum ex veri verterem abhorreant, id unum oportere intellegam nec.',
+       css: {
+         'text-align': 'justify',
+         'margin': '10px 13px 0px 7px'
+       }
+     }
+   };
 
-               $scope.data = sinAndCos();
+   $scope.data = sinAndCos();
 
-               /*Random Data Generator */
-               function sinAndCos() {
-                 var sin = [],sin2 = [],
-                     cos = [];
+   /*Random Data Generator */
+   function sinAndCos() {
+     var sin = [],sin2 = [],
+         cos = [];
 
-                 //Data is represented as an array of {x,y} pairs.
-                 for (var i = 0; i < 100; i++) {
-                   sin.push({x: i, y: Math.sin(i/10)});
-                   sin2.push({x: i, y: i % 10 == 5 ? null : Math.sin(i/10) *0.25 + 0.5});
-                   cos.push({x: i, y: 0.5 * Math.cos(i/10+ 2) + Math.random() / 10});
-                 }
+     //Data is represented as an array of {x,y} pairs.
+     for (var i = 0; i < 100; i++) {
+       sin.push({x: i, y: Math.sin(i/10)});
+       sin2.push({x: i, y: i % 10 == 5 ? null : Math.sin(i/10) *0.25 + 0.5});
+       cos.push({x: i, y: 0.5 * Math.cos(i/10+ 2) + Math.random() / 10});
+     }
 
-                 //Line chart data should be sent as an array of series objects.
-                 return [
-                   {
-                     values: sin,      //values - represents the array of {x,y} data points
-                     key: 'Sine Wave', //key  - the name of the series.
-                     color: '#ff7f0e'  //color - optional: choose your own line color.
-                   },
-                   {
-                     values: cos,
-                     key: 'Cosine Wave',
-                     color: '#2ca02c'
-                   },
-                   {
-                     values: sin2,
-                     key: 'Another sine wave',
-                     color: '#7777ff',
-                     area: true      //area - set to true if you want this line to turn into a filled area chart.
-                   }
-                 ];
-               }
+     //Line chart data should be sent as an array of series objects.
+     return [
+       {
+         values: sin,      //values - represents the array of {x,y} data points
+         key: 'Sine Wave', //key  - the name of the series.
+         color: '#ff7f0e'  //color - optional: choose your own line color.
+       },
+       {
+         values: cos,
+         key: 'Cosine Wave',
+         color: '#2ca02c'
+       },
+       {
+         values: sin2,
+         key: 'Another sine wave',
+         color: '#7777ff',
+         area: true      //area - set to true if you want this line to turn into a filled area chart.
+       }
+     ];
+   }
 
-               $scope.options2 = {
-                 chart: {
-                   type: 'pieChart',
-                   height: 500,
-                   x: function(d){return d.key;},
-                   y: function(d){return d.y;},
-                   showLabels: true,
-                   duration: 500,
-                   labelThreshold: 0.01,
-                   labelSunbeamLayout: true,
-                   legend: {
-                     margin: {
-                       top: 5,
-                       right: 35,
-                       bottom: 5,
-                       left: 0
-                     }
-                   }
-                 }
-               };
+   $scope.options2 = {
+     chart: {
+       type: 'pieChart',
+       height: 500,
+       x: function(d){return d.key;},
+       y: function(d){return d.y;},
+       showLabels: true,
+       duration: 500,
+       labelThreshold: 0.01,
+       labelSunbeamLayout: true,
+       legend: {
+         margin: {
+           top: 5,
+           right: 35,
+           bottom: 5,
+           left: 0
+         }
+       }
+     }
+   };
 
-               $scope.data2 = [
-                 {
-                   key: "One",
-                   y: 5
-                 },
-                 {
-                   key: "Two",
-                   y: 2
-                 },
-                 {
-                   key: "Three",
-                   y: 9
-                 },
-                 {
-                   key: "Four",
-                   y: 7
-                 },
-                 {
-                   key: "Five",
-                   y: 4
-                 },
-                 {
-                   key: "Six",
-                   y: 3
-                 },
-                 {
-                   key: "Seven",
-                   y: 0.5
-                 }
-               ];
-             }])
+   $scope.data2 = [
+     {
+       key: "One",
+       y: 5
+     },
+     {
+       key: "Two",
+       y: 2
+     },
+     {
+       key: "Three",
+       y: 9
+     },
+     {
+       key: "Four",
+       y: 7
+     },
+     {
+       key: "Five",
+       y: 4
+     },
+     {
+       key: "Six",
+       y: 3
+     },
+     {
+       key: "Seven",
+       y: 0.5
+     }
+   ];
+ }])
 ;
