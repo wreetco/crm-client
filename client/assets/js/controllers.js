f62ce59cc6250dd952262dd6b4e611e1a6a579e9
Jon <jonnmb@Gmail.com>
2016-08-13 15:07:06 -0600

    Logout - Top Margins - Tags Header Removal - Dropdown Close

--- client/assets/js/controllers.js
+++ client/assets/js/controllers.js
@@ -41,15 +41,18 @@ angular.module('application.controllers', ['nvd3'])
   $scope.logout = function(){
     $http.delete("http://burnsy.wreet.xyz/auth", { 'cron': true }).success(function(result) {
       $('#logout').closeModal();
-      //var target = angular.element('#materialize-lean-overlay-1');
-      //target.remove();
       delete window.sessionStorage.session;
       delete localStorage.contacts;
       delete localStorage.interface;
       $location.path('/');
       $window.location.reload();
     }).error(function(err) {
-      Materialize.toast('Please Try Again', 5000);
+      $('#logout').closeModal();
+      delete window.sessionStorage.session;
+      delete localStorage.contacts;
+      delete localStorage.interface;
+      $location.path('/');
+      $window.location.reload();
       console.log("error logging out: " + err);
     });
   };
@@ -86,7 +89,7 @@ angular.module('application.controllers', ['nvd3'])
     //activeTab.addClass("material-icons");
     activeTab.addClass("right");
     activeTab.addClass("activeTab");
-    activeTab.text(">");
+    activeTab.html("<i class=\"material-icons\" style=\"vertical-align: middle;\">chevron_right</i>" + activeTab.html());
     $(event.target).append(activeTab);
   }
   $("a.activatable").click(setActive);
@@ -668,12 +671,12 @@ angular.module('application.controllers', ['nvd3'])
     }
     $scope.contacts = contacts;
   }; // end filterBytag whatev methi
-  
+
   $scope.newDataSection = function() {
     $scope.current_fields.push({
       name: $('#new_section_text').val(),
       fields: []
-    })
+    });
     $('#new_section_text').val("");
     $('#new_section').css('display','none');
   }; // end newDataSection method
@@ -702,7 +705,7 @@ angular.module('application.controllers', ['nvd3'])
     } // end contact check
     if (!$scope.tags && $scope.contact)
       $scope.tags = Interface.getTags($scope.contacts);
-    $scope.$on('$routeChangeSuccess', function(next, current) { 
+    $scope.$on('$routeChangeSuccess', function(next, current) {
       if ($routeParams.tag)
         $scope.filterContactsByTag($routeParams.tag);
     });
@@ -837,147 +840,147 @@ angular.module('application.controllers', ['nvd3'])
 }])
 
 .controller('StatisticsController', ['$scope', function($scope) {
-   //This defines the graph on the stats page
-   $scope.new_contacts_opts = {
-     chart: {
-       type: 'lineChart',
-       height: 450,
-       margin : {
-         top: 20,
-         right: 20,
-         bottom: 40,
-         left: 55
-       },
-       x: function(d){ return d.x; },
-       y: function(d){ return d.y; },
-       useInteractiveGuideline: true,
-       dispatch: {
-         stateChange: function(e){ console.log("stateChange"); },
-         changeState: function(e){ console.log("changeState"); },
-         tooltipShow: function(e){ console.log("tooltipShow"); },
-         tooltipHide: function(e){ console.log("tooltipHide"); }
-       },
-       xAxis: {
-         axisLabel: 'day'
-       },
-       yAxis: {
-         axisLabel: 'contacts',
-         tickFormat: function(d){
-           return d3.format('d')(d);
-         },
-         axisLabelDistance: -10
-       },
-       callback: function(chart){
-         console.log("!!! lineChart callback !!!");
-       }
-     },
-     title: {
-       enable: true,
-       text: 'Title for Line Chart'
-     },
-     subtitle: {
-       enable: true,
-       text: 'Subtitle for simple line chart. Lorem ipsum dolor sit amet, at eam blandit sadipscing, vim adhuc sanctus disputando ex, cu usu affert alienum urbanitas.',
-       css: {
-         'text-align': 'center',
-         'margin': '10px 13px 0px 7px'
-       }
-     },
-     caption: {
-       enable: true,
-       html: '<b>Figure 1.</b> Lorem ipsum dolor sit amet, at eam blandit sadipscing, <span style="text-decoration: underline;">vim adhuc sanctus disputando ex</span>, cu usu affert alienum urbanitas. <i>Cum in purto erat, mea ne nominavi persecuti reformidans.</i> Docendi blandit abhorreant ea has, minim tantas alterum pro eu. <span style="color: darkred;">Exerci graeci ad vix, elit tacimates ea duo</span>. Id mel eruditi fuisset. Stet vidit patrioque in pro, eum ex veri verterem abhorreant, id unum oportere intellegam nec.',
-       css: {
-         'text-align': 'justify',
-         'margin': '10px 13px 0px 7px'
-       }
-     }
-   };
+  //This defines the graph on the stats page
+  $scope.new_contacts_opts = {
+    chart: {
+      type: 'lineChart',
+      height: 450,
+      margin : {
+        top: 20,
+        right: 20,
+        bottom: 40,
+        left: 55
+      },
+      x: function(d){ return d.x; },
+      y: function(d){ return d.y; },
+      useInteractiveGuideline: true,
+      dispatch: {
+        stateChange: function(e){ console.log("stateChange"); },
+        changeState: function(e){ console.log("changeState"); },
+        tooltipShow: function(e){ console.log("tooltipShow"); },
+        tooltipHide: function(e){ console.log("tooltipHide"); }
+      },
+      xAxis: {
+        axisLabel: 'day'
+      },
+      yAxis: {
+        axisLabel: 'contacts',
+        tickFormat: function(d){
+          return d3.format('d')(d);
+        },
+        axisLabelDistance: -10
+      },
+      callback: function(chart){
+        console.log("!!! lineChart callback !!!");
+      }
+    },
+    title: {
+      enable: true,
+      text: 'Title for Line Chart'
+    },
+    subtitle: {
+      enable: true,
+      text: 'Subtitle for simple line chart. Lorem ipsum dolor sit amet, at eam blandit sadipscing, vim adhuc sanctus disputando ex, cu usu affert alienum urbanitas.',
+      css: {
+        'text-align': 'center',
+        'margin': '10px 13px 0px 7px'
+      }
+    },
+    caption: {
+      enable: true,
+      html: '<b>Figure 1.</b> Lorem ipsum dolor sit amet, at eam blandit sadipscing, <span style="text-decoration: underline;">vim adhuc sanctus disputando ex</span>, cu usu affert alienum urbanitas. <i>Cum in purto erat, mea ne nominavi persecuti reformidans.</i> Docendi blandit abhorreant ea has, minim tantas alterum pro eu. <span style="color: darkred;">Exerci graeci ad vix, elit tacimates ea duo</span>. Id mel eruditi fuisset. Stet vidit patrioque in pro, eum ex veri verterem abhorreant, id unum oportere intellegam nec.',
+      css: {
+        'text-align': 'justify',
+        'margin': '10px 13px 0px 7px'
+      }
+    }
+  };
 
-   $scope.new_contacts_data = sinAndCos();
+  $scope.new_contacts_data = sinAndCos();
 
-   /*Random Data Generator */
-   function sinAndCos() {
-     var sin = [],sin2 = [],
-         cos = [];
+  /*Random Data Generator */
+  function sinAndCos() {
+    var sin = [],sin2 = [],
+        cos = [];
 
-     //Data is represented as an array of {x,y} pairs.
-     for (var i = 0; i < 100; i++) {
-       sin.push({x: i, y: Math.sin(i/10)});
-       sin2.push({x: i, y: i % 10 == 5 ? null : Math.sin(i/10) *0.25 + 0.5});
-       cos.push({x: i, y: 0.5 * Math.cos(i/10+ 2) + Math.random() / 10});
-     }
+    //Data is represented as an array of {x,y} pairs.
+    for (var i = 0; i < 100; i++) {
+      sin.push({x: i, y: Math.sin(i/10)});
+      sin2.push({x: i, y: i % 10 == 5 ? null : Math.sin(i/10) *0.25 + 0.5});
+      cos.push({x: i, y: 0.5 * Math.cos(i/10+ 2) + Math.random() / 10});
+    }
 
-     //Line chart data should be sent as an array of series objects.
-     return [
-       {
-         values: sin,      //values - represents the array of {x,y} data points
-         key: 'Sine Wave', //key  - the name of the series.
-         color: '#ff7f0e'  //color - optional: choose your own line color.
-       },
-       {
-         values: cos,
-         key: 'Cosine Wave',
-         color: '#2ca02c'
-       },
-       {
-         values: sin2,
-         key: 'Another sine wave',
-         color: '#7777ff',
-         area: true      //area - set to true if you want this line to turn into a filled area chart.
-       }
-     ];
-   }
+    //Line chart data should be sent as an array of series objects.
+    return [
+      {
+        values: sin,      //values - represents the array of {x,y} data points
+        key: 'Sine Wave', //key  - the name of the series.
+        color: '#ff7f0e'  //color - optional: choose your own line color.
+      },
+      {
+        values: cos,
+        key: 'Cosine Wave',
+        color: '#2ca02c'
+      },
+      {
+        values: sin2,
+        key: 'Another sine wave',
+        color: '#7777ff',
+        area: true      //area - set to true if you want this line to turn into a filled area chart.
+      }
+    ];
+  }
 
-   $scope.options2 = {
-     chart: {
-       type: 'pieChart',
-       height: 500,
-       x: function(d){return d.key;},
-       y: function(d){return d.y;},
-       showLabels: true,
-       duration: 500,
-       labelThreshold: 0.01,
-       labelSunbeamLayout: true,
-       legend: {
-         margin: {
-           top: 5,
-           right: 35,
-           bottom: 5,
-           left: 0
-         }
-       }
-     }
-   };
+  $scope.options2 = {
+    chart: {
+      type: 'pieChart',
+      height: 500,
+      x: function(d){return d.key;},
+      y: function(d){return d.y;},
+      showLabels: true,
+      duration: 500,
+      labelThreshold: 0.01,
+      labelSunbeamLayout: true,
+      legend: {
+        margin: {
+          top: 5,
+          right: 35,
+          bottom: 5,
+          left: 0
+        }
+      }
+    }
+  };
 
-   $scope.data2 = [
-     {
-       key: "One",
-       y: 5
-     },
-     {
-       key: "Two",
-       y: 2
-     },
-     {
-       key: "Three",
-       y: 9
-     },
-     {
-       key: "Four",
-       y: 7
-     },
-     {
-       key: "Five",
-       y: 4
-     },
-     {
-       key: "Six",
-       y: 3
-     },
-     {
-       key: "Seven",
-       y: 0.5
-     }
-   ];
- }])
+  $scope.data2 = [
+    {
+      key: "One",
+      y: 5
+    },
+    {
+      key: "Two",
+      y: 2
+    },
+    {
+      key: "Three",
+      y: 9
+    },
+    {
+      key: "Four",
+      y: 7
+    },
+    {
+      key: "Five",
+      y: 4
+    },
+    {
+      key: "Six",
+      y: 3
+    },
+    {
+      key: "Seven",
+      y: 0.5
+    }
+  ];
+}])
 ;
