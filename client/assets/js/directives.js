a8fd0d72a70bba50014d0f8a2d50dfe767a5524b
Jon <jonnmb@Gmail.com>
2016-08-17 10:45:15 -0600

    edits

--- client/assets/js/directives.js
+++ client/assets/js/directives.js
@@ -1,91 +1,91 @@
- angular.module('application.directives', [])
-  
+angular.module('application.directives', [])
+
 .directive("contenteditable", function() {
-return {
-  restrict: "A",
-  require: "ngModel",
-  link: function(scope, element, attrs, ngModel) {
+  return {
+    restrict: "A",
+    require: "ngModel",
+    link: function(scope, element, attrs, ngModel) {
 
-    function read() {
-      ngModel.$setViewValue(element.html());
+      function read() {
+        ngModel.$setViewValue(element.html());
+      }
+
+      ngModel.$render = function() {
+        element.html(ngModel.$viewValue || "");
+      };
+
+      element.bind("blur keyup change", function() {
+        scope.$apply(read);
+      });
     }
-
-    ngModel.$render = function() {
-      element.html(ngModel.$viewValue || "");
-    };
-
-    element.bind("blur keyup change", function() {
-      scope.$apply(read);
-    });
-  }
-};
+  };
 })
 
 //Thanks to marko on StackOverflow https://jsfiddle.net/200eoamf/1/
 .directive('onReadFile', function ($parse) {
-	return {
-		restrict: 'A',
-		scope: false,
-		link: function(scope, element, attrs) {
-			element.bind('change', function(e) {
-			var onFileReadFn = $parse(attrs.onReadFile);
-			var reader = new FileReader();
-			reader.onload = function() {
-				var fileContents = reader.result;
-				// invoke parsed function on scope
-				// special syntax for passing in data
-				// to named parameters
-				// in the parsed function
-				// we are providing a value for the property 'contents'
-				// in the scope we pass in to the function
-				scope.$apply(function() {
-					onFileReadFn(scope, {
-						'contents' : fileContents
-					});
-				});
-			};
-			reader.readAsText(element[0].files[0]);
-			});
-		}
-	};
+  return {
+    restrict: 'A',
+    scope: false,
+    link: function(scope, element, attrs) {
+      element.bind('change', function(e) {
+        var onFileReadFn = $parse(attrs.onReadFile);
+        var reader = new FileReader();
+        reader.onload = function() {
+          var fileContents = reader.result;
+          // invoke parsed function on scope
+          // special syntax for passing in data
+          // to named parameters
+          // in the parsed function
+          // we are providing a value for the property 'contents'
+          // in the scope we pass in to the function
+          scope.$apply(function() {
+            onFileReadFn(scope, {
+              'contents' : fileContents
+            });
+          });
+        };
+        reader.readAsText(element[0].files[0]);
+      });
+    }
+  };
 })
 
 
 .directive('wrmn', function($compile) {
-	return {
-		restrict: 'A',
-		scope: false,
-		link: function($scope, $el) {
-			if (!wrmn.is_initd) wrmn.init(document.getElementById('wmenu'), {menu_width: document.body.offsetWidth * .7, width_offset: 33});
-		}
-	};
+  return {
+    restrict: 'A',
+    scope: false,
+    link: function($scope, $el) {
+      if (!wrmn.is_initd) wrmn.init(document.getElementById('wmenu'), {menu_width: document.body.offsetWidth * .7, width_offset: 33});
+    }
+  };
 })
 
 .directive('wrmnToggleMenu', function($templateRequest, $compile) {
-	return function($scope, $el, $atts) {
-		$templateRequest('views/_contact_info.html').then(function(html){
-			var tmp = angular.element(html);
-			$("#__wrmn_right").empty();
-			$("#__wrmn_right").append(tmp);
-			$compile(tmp)($scope);
-		});
-	};
+  return function($scope, $el, $atts) {
+    $templateRequest('views/_contact_info.html').then(function(html){
+      var tmp = angular.element(html);
+      $("#__wrmn_right").empty();
+      $("#__wrmn_right").append(tmp);
+      $compile(tmp)($scope);
+    });
+  };
 })
 
 .directive('wrmnToggleMenu', function() {
-	return {
-		scope: {
-			current_contact: "=wrmnToggleMenu"
-		},
-		link: function($scope, $el) {
-			$el.on('click', function() {
-				$scope.$apply(function() {
-					$scope.$parent.$parent.infoBar($scope.current_contact);
-					wrmn.cron.toggleMenu('right');
-				});
-			});
-		}
-	};
+  return {
+    scope: {
+      current_contact: "=wrmnToggleMenu"
+    },
+    link: function($scope, $el) {
+      $el.on('click', function() {
+        $scope.$apply(function() {
+          $scope.$parent.$parent.infoBar($scope.current_contact);
+          wrmn.cron.toggleMenu('right');
+        });
+      });
+    }
+  };
 })
 
 .directive('wEditable', function($timeout, $compile) {
@@ -97,45 +97,47 @@ return {
       $scope.save = function() {
         //save that shit breh
         console.log("save");
-        var newval = $scope.wrapper_element.children().first().val();
+        var newval = $scope.element.children().first().val();
         //empty the wedit span
-        $scope.wrapper_element.empty();
+        $scope.element.empty();
         //put in the new value
-        $scope.wrapper_element.text(newval);
+        $scope.element.text(newval);
+        $scope.element[0].addEventListener("click", $scope.listener, false);
       };
       $scope.cancel = function() {
         // don't change anything just empty and replace
         console.log("cancel");
         //empty the wedit span
-        $scope.wrapper_element.empty();
+        $scope.element.empty();
         //reset the value/model
         $scope.obj = $scope.orig;
         //put in the original value
-        $scope.wrapper_element.text($scope.obj);
+        $scope.element.text($scope.obj);
+        $scope.element[0].addEventListener("click", $scope.listener, true);
       };
-    },
-    link: function($scope, $el) {
-      $el.click(function() {
+      $scope.listener = function(){
         //ID for each one
-        $scope.id = $el.attr("w-editable");
+        $scope.id = $scope.element.attr("w-editable");
         var editz = "<input type='text' ng-model='obj' autofocus id='" + $scope.id + "' />" +
-                    "<i class='material-icons green-check' style='position: absolute; top: 1em; right: 1.5em;' ng-click='save();'>check</i>" +
-                    "<i class='material-icons red-text' style='position: absolute; top: 1em; right: .5em;' ng-click='cancel();'>cancel</i>";
+            "<i class='material-icons green-check' style='position: absolute; top: 1em; right: 1.5em;' ng-click='save();'>check</i>" +
+            "<i class='material-icons red-text' style='position: absolute; top: 1em; right: .5em;' ng-click='cancel();'>cancel</i>";
         //I need to keep a copy of the original value
         $scope.orig = $scope.obj;
         //Lets keep track of our parent element
-        $scope.wrapper_element = $el;
         editz = angular.element(editz);
         // clear the parent
-        $el.empty();
+        $scope.element.empty();
         // add div to the dom
-        $el.append($compile(editz)($scope));
+        $scope.element.append($compile(editz)($scope));
         // we need to get rid of the attatched handler or you cant select the input without firing off a new click event
-        console.log($el);
-        $el.off();
-      });
+        console.log($scope.element);
+        //remove event listener after swapping in the input deal
+      };
+    },
+    link: function($scope, $el) {
+      //add element to scope so we can like do things with it
+      $scope.element = $el;
+      //add the even listener
+      $scope.element[0].addEventListener("click", $scope.listener, true);
     }
-  };
-})
-	
-;		
+  }});
